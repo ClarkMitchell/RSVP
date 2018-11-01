346fe825964951695c4c44d5ad74cd1a47120916
Clark Mitchell <ccmitche@gleim.com>
2018-11-01 12:03:46 -0600

    remove var_dump

--- src/Service/Pipeline.php
+++ src/Service/Pipeline.php
@@ -43,7 +43,6 @@ class Pipeline
             $response = $this->headCount->getResponse();
         } elseif ($message === 'no rsvp') {
             $response = $this->noRsvp->getResponse();
-            var_dump($response);die();
         } else {
             $this->guest->setGuestName($message);
             $this->guest->setPhone($phone);
